bc0ca83848790b5181dfa3d308ae17a29c958b20
Benjamin Skinner <btskinner@virginia.edu>
2017-10-26 18:19:21 -0400

    new filter feature, visible-only list, in alpha order

--- index.md
+++ index.md
@@ -9,11 +9,25 @@ custom_js:
 - school_array
 ---
 
-<div class='sidebar'>
-  <div class='heading'>
-    <h1>Information</h1>
-  </div>
-  <div id='info' class='info'></div>
-</div>
+<!-- <div class='sidebar'> -->
+<!--   <div class='heading'> -->
+<!--     <h1>Information</h1> -->
+<!--   </div> -->
+<!--   <div id='info' class='info'></div> -->
+<!-- </div> -->
 
-<div id='map' class='map pad2'></div>
+<!-- <div id='map' class='map pad2'></div> -->
+
+<div id='map'></div>
+
+<div class='map-overlay'>
+	<div>
+		<fieldset>
+			<input id='search-filter' type='text' placeholder='TODO: Search for schools by name or location' />
+		</fieldset>
+		<fieldset style='background-color:#E57200;'>
+			<input id='feature-filter' type='text' placeholder='Filter results by name' />
+		</fieldset>
+	</div>	
+    <div id='feature-listing' class='listing'></div>
+</div>
--- js/index.map.js
+++ js/index.map.js
@@ -11,17 +11,11 @@
 // e := frpl pct (hs)
 // f := student / counselor ratio (hs)
 
-// This will let you use the .remove() function later on
-if (!('remove' in Element.prototype)) {
-  Element.prototype.remove = function() {
-    if (this.parentNode) {
-      this.parentNode.removeChild(this);
-    }
-  };
-}
+// INIT MAP ----------------------------------------------------------
 
 // public token
-mapboxgl.accessToken = 'pk.eyJ1IjoiYnRza2lubmVyIiwiYSI6ImNqOTRhbzk1MTFhajIzNXFiMTdndnVsazEifQ.SHgqzAKZ52DTOCjGVMJgPw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnRza2lubmVyIiwiYSI6ImNqOTRhbzk1M'
+    + 'TFhajIzNXFiMTdndnVsazEifQ.SHgqzAKZ52DTOCjGVMJgPw';
 
 // init map
 var map = new mapboxgl.Map({
@@ -31,24 +25,175 @@ var map = new mapboxgl.Map({
     zoom: 4
 });
 
+// VARIABLES ---------------------------------------------------------
+
+// init popup 
+var popup = new mapboxgl.Popup({
+    closeButton: false
+});
+
+// init structure to hold visible points
+var visible = [];
+
+// init message variable
+var messages = ['Zoom or drag the map to populate results',
+		'Check spelling or drag the map to re-populate results'];
+
+// get HTML elements for listing and filter bar
+var filterEl = document.getElementById('feature-filter');
+var listingEl = document.getElementById('feature-listing');
+
+// init filter to be hidden
+filterEl.parentNode.style.display = 'none';
+
+// init filtering switch
+var filterSwitch = false;
+
+// GENERAL FUNCTIONS -------------------------------------------------
+
+// adds .remove() function if using old browser 
+if (!('remove' in Element.prototype)) {
+  Element.prototype.remove = function() {
+    if (this.parentNode) {
+      this.parentNode.removeChild(this);
+    }
+  };
+}
+
+// trim and lower string
+function normalize(string) {
+    return string.trim().toLowerCase();
+}
+
+// MAPPING FUNCTIONS -------------------------------------------------
+
+// primary function to populate listings 
+function renderListings(features) {
+
+    // clear any existing listings
+    listingEl.innerHTML = '';
+
+    // if there are items visible on the map...
+    if (features.length) {
+
+	// return filter text to black
+	filterEl.style.color = '#000';
+
+	// sort so that they are listed alphabetically in list
+	features.sort(function(x,y) {
+	    
+	    if(s[x.id].a < s[y.id].a) return -1;
+	    if(s[x.id].a > s[y.id].a) return 1;
+	    return 0; 
+
+	});
+	
+        features.forEach(function(feature) {
+
+	    // var schname = getProperty(s, a, id);
+	    var schname = s[feature.id].a;
+            var item = document.createElement('p');
+
+            item.textContent = schname;
+            item.addEventListener('mouseover', function() {
+                // add popup when mousing over
+                popup.setLngLat(feature.geometry.coordinates)
+                    .setText(schname)
+                    .addTo(map);
+            });
+	    item.addEventListener('mouseleave', function() {
+                // remove popup on mouseleave
+                popup.remove();
+            });
+	    
+            listingEl.appendChild(item);
+        });
+
+        // show the filter input
+        filterEl.parentNode.style.display = 'block';
+
+	// ...otherwise
+    } else {
+
+	var empty = document.createElement('p');
+        
+        // hide the filter input
+	if (filterSwitch) {
+	    // change color of input text
+	    filterEl.style.color = getColor(rtorange);
+	    message = messages[1];
+	    listingEl.appendChild(empty);
+	} else {
+	    filterEl.parentNode.style.display = 'none';
+	    message = messages[0]
+	}
+
+	empty.textContent = message;
+	
+        // remove features filter
+        map.setFilter('schools', ['has', '$id']);
+    }
+}
+
+// only want unique items in the list
+function getUniqueFeatures(array, comparatorProperty) {
+
+    // init dictionary that will associate IDs with T/F
+    var existingFeatureKeys = {};
+
+    var uniqueFeatures = array.filter(function(el) {
+        if (existingFeatureKeys[el.properties[comparatorProperty]]) {
+            return false;
+        } else {
+            existingFeatureKeys[el.properties[comparatorProperty]] = true;
+            return true;
+        }
+    });
+
+    return uniqueFeatures;
+}
+
+// only want unique items in the list (by ID)
+function getUniqueFeaturesByID(array) {
+
+    // init dictionary that will associate IDs with T/F
+    var existingFeatureKeys = {};
+
+    var uniqueFeatures = array.filter(function(el) {
+        if (existingFeatureKeys[el.id]) {
+            return false;
+        } else {
+            existingFeatureKeys[el.id] = true;
+            return true;
+        }
+    });
+
+    return uniqueFeatures;
+}
+
+function addToVisible() {
+
+    filterSwitch = false;
+    map.setFilter('schools', ['has', '$id']);
+    
+    var features = map.queryRenderedFeatures({layers:['schools']});
+    
+    if (features) {
+        var uniqueFeatures = getUniqueFeaturesByID(features, 'id');
+        // Populate features for the listing overlay.
+	renderListings(uniqueFeatures);
+	
+        // Clear the input container
+        filterEl.value = '';
+	
+        // Store the current features in school variable to
+        // later use for filtering on `keyup`.
+	visible = uniqueFeatures;
+    }
+}
+
 // load map
 map.on('load', function () {
-
-    // CONTROLS ------------------------------------------------------
-    
-    // add geolocate control to the map.
-    map.addControl(new mapboxgl.GeolocateControl({
-    	positionOptions: {
-            enableHighAccuracy: true
-    	},
-    	fitBoundsOptions: {
-    	    maxZoom: 12
-    	},
-    	trackUserLocation: false
-    }));
-
-    // Add zoom and rotation controls to the map.
-    map.addControl(new mapboxgl.NavigationControl());
 
     // DATA ----------------------------------------------------------
 
@@ -97,69 +242,80 @@ map.on('load', function () {
 
     // SIDEBAR -------------------------------------------------------
 
-    buildSchoolInfoList(data);
-
-    var visible = [];
+    // add to visible variable on these events, which triggers sidebar
+    map.on('zoomstart', function() {
+	map.setFilter('schools', ['has', '$id']);
+    });
+    map.on('movestart', function() {
+	map.setFilter('schools', ['has', '$id']);
+    });
+    map.on('zoomend', addToVisible);
+    map.on('moveend', addToVisible);
 
     // POPUPS --------------------------------------------------------
 
-    // Add an event listener for when a user clicks on the map
-    map.on('click', function(e) {
-	// Query all the rendered points in the view
-	var features = map.queryRenderedFeatures(e.point, { layers: ['schools'] });
-	if (features.length) {
-	    var clickedPoint = features[0];
-	    // 1. fly to the point
-	    flyToSchool(clickedPoint);
-	    // 2. close all other popups and display popup for clicked school
-	    createPopUp(clickedPoint);
-	    // 3. highlight listing in sidebar (and remove highlight for all other listings)
-	    var activeItem = document.getElementsByClassName('active');
-	    if (activeItem[0]) {
-		activeItem[0].classList.remove('active');
-	    }
-	    // Find the index of the school that corresponds to the clickedPoint
-	    // that fired the event listener
-	    var selectedFeature = clickedPoint.id;
-	    
-	    for (var i = 0; i < data.features.length; i++) {
-		if (data.features[i].id === selectedFeature) {
-		    selectedFeatureIndex = i;
-		}
-	    }
-	    // Select the correct list item using the found index and add the active class
-	    var listing = document.getElementById('info-' + selectedFeatureIndex);
-	    listing.classList.add('active');
-	}
+    map.on('mousemove', 'schools', function(e) {
+        // change the cursor style to pointer finger
+        map.getCanvas().style.cursor = 'pointer';
+
+        // populate the popup and set its coordinates based on the feature
+        var feature = e.features[0];
+        popup.setLngLat(feature.geometry.coordinates)
+	    .setText(s[feature.id].a)
+            .addTo(map);
     });
 
+    map.on('mouseleave', 'schools', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+    });
+
+    // Call this function on initialization
+    // passing an empty array to render an empty state
+    renderListings(visible);
     
+    // CONTROLS ------------------------------------------------------
+
+    // filter box
+    filterEl.addEventListener('keyup', function(e) {
+
+	filterSwitch = true;
+	
+        var value = normalize(e.target.value);
+
+        // remove visible features that don't match the input value.
+        var filtered = visible.filter(function(feature) {
+            var name = normalize(s[feature.id].a);
+            // return name.indexOf(value) > -1 || code.indexOf(value) > -1;
+	    return name.indexOf(value) > -1
+        });
+
+        // populate the sidebar with filtered results
+        renderListings(filtered);
+
+        // set the filter to populate features into the layer
+        map.setFilter('schools', ['in', '$id'].concat(filtered.map(function(feature) {
+            return feature.id;
+        })));
+	popup.remove();
+
+    });
     
-    // // create popup
-    // var popup = new mapboxgl.Popup({
-    //     closeButton: false,
-    //     closeOnClick: false
-    // });
+    // add geolocate control to the map.
+    map.addControl(new mapboxgl.GeolocateControl({
+    	positionOptions: {
+            enableHighAccuracy: true
+    	},
+    	fitBoundsOptions: {
+    	    maxZoom: 12
+    	},
+    	trackUserLocation: false
+    }));
 
-    // // when hovering...
-    // map.on('mouseenter', 'schools', function(e) {
-    //     // change cursor to pointer
-    //     map.getCanvas().style.cursor = 'pointer';
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
 
-    //     // add popup with name
-    //     popup.setLngLat(e.features[0].geometry.coordinates)
-    //         .setHTML(s[e.features[0].id].a)
-    //         .addTo(map);
-    // });
-
-    // // ...when leaving
-    // map.on('mouseleave', 'schools', function() {
-    // 	// return cursor to normal
-    //     map.getCanvas().style.cursor = '';
-    // 	// remove popup
-    //     popup.remove();
-    // });
-  
+ 
 });
 
     
