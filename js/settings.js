2bc321f5c3639c1b907e1784547b457f8b67fbc5
Benjamin Skinner <btskinner@virginia.edu>
2017-11-26 11:15:00 -0500

    version 4

--- js/settings.js
+++ js/settings.js
@@ -2,3 +2,10 @@
 var jeffblue = {r:'35',g:'45',b:'75',a:'1'};
 var rtorange = {r:'229',g:'114',b:'0',a:'1'};
 
+// sector
+var sector = {'1':'Public four-year',
+	      '2':'Private, not-for-profit four-year',
+	      '4':'Public two-year',
+	      '5':'Private, not-for-profit two-year'}
+
+
